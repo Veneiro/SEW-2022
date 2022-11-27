40fd217caa7d660e0bc25f27c8348785334c75ba
Veneiro <50624734+Veneiro@users.noreply.github.com>
2022-11-27 22:25:49 +0100

    started secction 2 of practice 3

---
+++ Practica-3.2/Tarea1/Ejercicio7.js
@@ -0,0 +1,11 @@
+class Ej7{
+    hideText(){
+        $("p").hide();
+    }
+
+    showText(){
+        $("p").show();
+    }
+}
+
+ej7 = new Ej7();
---
+++ Practica-3.2/Tarea4/Ejercicio7.js
@@ -0,0 +1,7 @@
+class Ej7{
+    removeParagraph(){
+        $("p").detach();
+    }
+}
+
+ej7 = new Ej7();
