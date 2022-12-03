c62006cdd6e0bc1115b85979ea248d18a1fa76fa
Veneiro <50624734+Veneiro@users.noreply.github.com>
2022-12-03 21:29:44 +0100

    update

--- mainPage.js
+++ mainPage.js
@@ -22,6 +22,59 @@ class Index{
     ejercicio6(){
         window.location.href = './Práctica-3.1/Ejercicio-06/Tarea1/CalculadoraRpn.html';
     }
+
+    ejercicio7(){
+        window.location.href = './Practica-3.2/Ejercicio-07/Tarea1/Ejercicio7.html';
+    }
+
+    ejercicio72(){
+        window.location.href = './Practica-3.2/Ejercicio-07/Tarea2/Ejercicio7.html';
+    }
+
+    ejercicio73(){
+        window.location.href = './Practica-3.2/Ejercicio-07/Tarea3/Ejercicio7.html';
+    }
+
+    ejercicio74(){
+        window.location.href = './Practica-3.2/Ejercicio-07/Tarea4/Ejercicio7.html';
+    }
+    
+    ejercicio75(){
+        window.location.href = './Practica-3.2/Ejercicio-07/Tarea5/Ejercicio7.html';
+    }
+
+    ejercicio76(){
+        window.location.href = './Practica-3.2/Ejercicio-07/Tarea6/Ejercicio7.html';
+    }
+
+    ejercicio8(){
+        window.location.href = './Practica-3.2/Ejercicio-08/Tarea1/Ejercicio8.html';
+    }
+
+    ejercicio9(){
+        window.location.href = './Practica-3.2/Ejercicio-09/Tarea1/Ejercicio9.html';
+    }
+    
+    ejercicio10(){
+        window.location.href = './Practica-3.2/Ejercicio-10/Tarea1/Ejercicio10.html';
+    }
+
+    ejercicio11(){
+        window.location.href = './Practica-3.2/Ejercicio-11/Tarea1/Ejercicio11.html';
+    }
+
+    ejercicio12(){
+        window.location.href = './Practica-3.2/Ejercicio-12/Tarea1/Ejercicio12.html';
+    }
+
+    ejercicio13(){
+        window.location.href = './Practica-3.2/Ejercicio-13/Tarea1/Ejercicio13.html';
+    }
+
+    ejercicio14(){
+        window.location.href = './Practica-3.2/Ejercicio-14/Tarea1/Ejercicio14.html';
+    }
+
     exercise1Code(){
         var tag_id = document.getElementById('html');
         document.getElementById("Exercise1Txt").value = tag_id.innerHTML;
