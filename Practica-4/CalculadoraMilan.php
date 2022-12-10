f72ea946e713c3eeeb6e1c23f7c8977cc35e6fdd
Veneiro <50624734+Veneiro@users.noreply.github.com>
2022-12-10 15:37:43 +0100

    update

--- Practica-4/CalculadoraMilan.php
+++ Practica-4/CalculadoraMilan.php
@@ -1,56 +1,108 @@
+<!DOCTYPE html>
+<html>
 
-/**
- * Definición de la clase CalculadoraDiabetica
- */
+<head>
+    <title>Ejemplo</title>
+    <link rel="stylesheet" type="text/css" href="./CalculadoraMilán.css" />
+</head>
 
-class CalculadoraDiabetica {
-    protected $glucosa;
-    protected $glucosilada;
+<body>
 
-    public function __construct($glucosa){
-        $this->glucosa = floatval($glucosa); //Convierte un string en un float
-        $this->glucosilada = (46.7 + $this->glucosa) / 28.7 ;
+    <h3>Pulse un botón</h3>
+    <form action='#' method='post' name='botones'>
+        <div>
+            <input type='submit' class='button' name='boton1' value='Botón 1' />
+            <input type='submit' class='button' name='boton2' value='Botón 2' />
+            <input type='submit' class='button' name='boton3' value='Botón 3' />
+        </div>
+        <div>
+            <input type='submit' class='button' name='boton4' value='Botón 4' />
+            <input type='submit' class='button' name='boton5' value='Botón 5' />
+            <input type='submit' class='button' name='boton6' value='Botón 6' />
+        </div>
+        <div>
+            <input type='submit' class='button' name='boton7' value='Botón 7' />
+            <input type='submit' class='button' name='boton8' value='Botón 8' />
+            <input type='submit' class='button' name='boton9' value='Botón 9' />
+        </div>
+    </form>
+    <form action='#' method='post' name='calculadora'>
+        <input type='text' name='expresion' value='' />
+        <input type='submit' value='Calcular' />
+    </form>
+    <?php
+    $expresion = "";
+    $resultado = "";
+
+    $formularioPOST = "";
+
+    class Botonera
+    {
+        protected $mensaje;
+
+        public function __construct()
+        {
+            $this->mensaje = "";
+        }
+
+        public function getMensaje()
+        {
+            //devuelve el mensaje
+            return $this->mensaje;
+        }
+
+        public function pulsarBoton($boton)
+        {
+            // Construye el mensaje con el botón pulsado, la fecha y la hora
+            $this->mensaje = "Se ha pulsado: " . $boton . " en el instante: " . date('l jS \of F Y h:i:s A');
+        }
     }
-    
-    public function getGlucosa(){
-         //devuelve la glucosa
-         return $this->glucosa;   
+
+    $aviso = "No se ha pulsado ningún botón";
+
+    //Solo se ejecutará si se ha pulsado un botón
+    if (count($_POST) > 0) {
+        $miBotonera = new Botonera();
+
+        if (isset($_POST['boton1']))
+            $_POST["expresion"] += "1";
+        if (isset($_POST['boton2']))
+            $_POST["expresion"] += "2";
+        if (isset($_POST['boton3']))
+            $miBotonera->pulsarBoton("Botón 3");
+        if (isset($_POST['boton4']))
+            $miBotonera->pulsarBoton("Botón 4");
+        if (isset($_POST['boton5']))
+            $miBotonera->pulsarBoton("Botón 5");
+        if (isset($_POST['boton6']))
+            $miBotonera->pulsarBoton("Botón 6");
+        if (isset($_POST['boton7']))
+            $miBotonera->pulsarBoton("Botón 7");
+        if (isset($_POST['boton8']))
+            $miBotonera->pulsarBoton("Botón 8");
+        if (isset($_POST['boton9']))
+            $miBotonera->pulsarBoton("Botón 9");
+
+        $aviso = $miBotonera->getMensaje();
     }
+    echo "<p>aviso = " . $aviso . "</p>";
 
-    public function getGlucosilada(){
-         //devuelve la glucosilada
-         return $this->glucosilada;   
+    //Solo se ejecutará si se han enviado los datos desde el formulario al pulsar el boton Calcular
+    if (count($_POST) > 0) {
+        $formularioPOST = $_POST;
+
+        $expresion = $_POST["expresion"];
+        try {
+            $resultado = eval("return $expresion ;");
+            printf("<p> Operación -> %s </p>", $expresion);
+            printf("<p> Resultado = %d </p>", $resultado);
+        } catch (Exception $e) {
+            $resultado = "Error: " . $e->getMessage();
+        }
     }
-}
+    ?>
 
-$miGlucosa = 0.0;
-$miGlucosilada = 0.0;
 
-//Solo se ejecutará si se han enviado los datos desde el formulario al pulsar el boton Calcular
-if (count($_POST)>0) 
-    {   
-        $miCalculadora = new CalculadoraDiabetica($_POST["glucosa"]);
-        $miGlucosa = $miCalculadora->getGlucosa();
-        $miGlucosilada = $miCalculadora->getGlucosilada();
-    }
+</body>
 
-// Interfaz con el usuario. En el interior de comillas dobles se deben usar comillas simples
-echo "  
-        <link href="./CalculadoraMilán.css">
-        <h3>Convierte la glucosa media en Hemoglobina glucosilada</h3>
-        <h4>Rango de validez de la conversión: valores entre 70 y 500 de glucosa media en mg/dL</h4>
-
-        <form action='#' method='post' name='calculadora'>
-            <p>Glucosa media en los últimos 90 días (mg/dL):
-                <input type='text' name='glucosa' value='$miGlucosa'/>
-            </p>
-            <p>Hemoglobina glucosilada (%):
-                <input type='text' name='glucosilada' value='$miGlucosilada' readonly/>
-            </p>           
-            <input type='submit'  value='Calcular'/>
-        </form>
-    ";
-
-echo "<h3>Variables</h3>";
-echo "<p>miGlucosa = " . $miGlucosa . " { var_dump() = "; var_dump($miGlucosa); echo" }</p>";
-echo "<p>miGlucosilada = " . $miGlucosilada . " { var_dump() = "; var_dump($miGlucosilada); echo" }</p>";
+</html>
