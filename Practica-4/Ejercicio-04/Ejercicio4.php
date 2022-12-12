8352dca6b66b510fe86991025c0d482e99b990d0
Veneiro <veneiroyt@gmail.com>
2022-12-12 22:38:46 +0100

    update

--- Practica-4/Ejercicio-04/Ejercicio4.php
+++ Practica-4/Ejercicio-04/Ejercicio4.php
@@ -8,38 +8,92 @@
 
 <body>
     <?php
-    // 100-CodigoPHP.php
-    // Versión 1.0 10/12/2017 Juan Manuel Cueva Lovelle. Universidad de Oviedo
-
-    $apikey = "d204d0030eb6607d27cc0fd1569ca4f7";
-    $url = "https://api.metalpriceapi.com/v1/latest?api_key=" . $apikey;
+    session_start();
+    $apikey = "";
+    $url = "" . $apikey;
 
     // Se solicita el archivo JSON de la url que se pasa como parámetro y se recibe como un string
-    $datos = file_get_contents($url);
-    echo "<h2>JSON recibido</h2>";
+    //$datos = file_get_contents($url);
+    class Compute
+    {
+        public function __construct()
+        {
+            if (count($_POST) > 0) {
+                if (isset($_POST['Buscar'])) {
+                    if(isset($_GET['startDate']) && isset($_GET['endDate'])){
+                        echo "llegó";
+                        echo $_GET['startDate'];
+                        echo $_GET['endDate'];
+                    }
+                }
+            }
+        }
 
-    // Muestra el archivo JSON recibido como string maquetado en bonito
-    echo "<pre>";
-    $jsonMaquetado = json_decode($datos, JSON_PRETTY_PRINT);
-    print_r($jsonMaquetado);
-    echo "</pre>";
+        public function compute(){
+            // Se convierte el JSON en un objeto PHP
+            $json = 'json_decode($datos)';
 
-    // Se convierte el JSON en un objeto PHP
-    $json = json_decode($datos);
+            if ($json == null) {
+                echo "<h3>Error en el archivo JSON recibido</h3>";
+            } else {
+                echo "<h3>Precio actual del oro en Euros</h3>";
+            }
 
-    if ($json == null) {
-        echo "<h3>Error en el archivo JSON recibido</h3>";
-    } else {
-        echo "<h3>JSON decodificado correctamente</h3>";
+            # Datos contenidos en el JSON
+            foreach ($json as $key => $value) {
+                if (gettype($value) == "array" || gettype($value) == "object") {
+                    foreach ($value as $keyval => $val) {
+                        if ($keyval == "EUR") {
+                            echo $keyval . "<br>";
+                            echo "<p>" . $val . "</p>";
+                        }
+                    }
+                }
+            }
+
+            $url2 = "https://api.metalpriceapi.com/v1/timeframe?api_key= &start_date=&end_date=&base=USD&currencies=EUR";
+
+            $datos2 = file_get_contents($url2);
+            echo "<h2>Precios del oro en tiempo real de todas las divisas</h2>";
+
+            // Se convierte el JSON en un objeto PHP
+            $json2 = json_decode($datos2);
+
+            if ($json2 == null) {
+                echo "<h3>Error en el archivo JSON recibido</h3>";
+            } else {
+                echo "<h3>Precio actual del oro en Euros</h3>";
+            }
+
+            $array = array();
+            # Datos contenidos en el JSON
+            foreach ($json2 as $key => $value) {
+                if (gettype($value) == "array" || gettype($value) == "object") {
+                    foreach ($value as $keyval => $val) {
+                        echo $keyval . "<br>";
+                        foreach ($val as $key => $v) {
+                            echo "<p>" . trim($v) . "</p>";
+                            array_push($array, array($keyval, trim($v)));
+                        }
+                    }
+                }
+            }
+        }
     }
-
-    # Datos contenidos en el JSON
-    $precio = $json->rates;
-
-    foreach($precio as $p){
-        echo $p;
-        echo "\r";
-    }
+    $compute = new Compute();
+    echo "<h2>Precios del oro en tiempo real de todas las divisas</h2>";
+    echo "<p> Introduce la fecha de inicio </p>";
+    echo "<form action='#' method='get'>
+    <input name='startDate' type='text' placeholder='YYYY-MM-DD'/>
+    </form>";
+    echo "<p> Introduce la fecha de fin </p>";
+    echo "<form action='#' method='get'>
+    <input name='endDate' type='text' placeholder='YYYY-MM-DD'/>
+    </form>";
+    echo "<form action='#' method='post'>
+    <input type='submit' name='Buscar' value='Buscar'/>
+    </form>";
+    
     ?>
 </body>
 
